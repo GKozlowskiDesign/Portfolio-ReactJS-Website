462fc7bdd7afa8ab406c71ce9227f1b33dc7ec6b
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-08 02:33:48 -0400

    Update Experience.jsx

--- src/componets/Experience.jsx
+++ src/componets/Experience.jsx
@@ -82,7 +82,7 @@ const Experience = () => {
 
   return(
 
-  <div name="experience" className="w-full h-screen pt-20">
+  <div name="experience" className="w-full h-screen pt-40 pb-20">
     <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
     <div>
         <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
