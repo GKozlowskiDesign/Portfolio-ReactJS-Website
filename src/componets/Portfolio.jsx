faf55ad1436a3f393326d0008040859a98c37a0e
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-08 02:32:27 -0400

    Update Portfolio.jsx

--- src/componets/Portfolio.jsx
+++ src/componets/Portfolio.jsx
@@ -60,7 +60,7 @@ const Portfolio = () => {
     ]
 
   return (
-    <div name="portfolio" className="w-full text-white md:h-screen pb-20">
+    <div name="portfolio" className="w-full text-white md:h-screen pb-20 mb-10">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="p-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
