165f54d4ccc940f0418cdfa4e5e73a71aaac184c
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-21 15:12:33 -0400

    Update Experience.jsx

--- src/componets/Experience.jsx
+++ src/componets/Experience.jsx
@@ -116,7 +116,7 @@ const Experience = () => {
     <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
     <div>
         <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
-        <p className="py-6">These are technologies I frequently use. </p>
+        <p className="py-6">These are technologies I frequently use! </p>
     </div>
     <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
     {
