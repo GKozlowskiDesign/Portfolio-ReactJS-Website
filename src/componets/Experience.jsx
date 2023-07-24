1971899ac201cda86e75a600779ba1d28e10d8d9
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-24 17:42:54 -0400

    Update Experience.jsx

--- src/componets/Experience.jsx
+++ src/componets/Experience.jsx
@@ -118,12 +118,12 @@ const Experience = () => {
         <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
         <p className="py-6">These are technologies I frequently use! </p>
     </div>
-    <div className="w-full border-2 border-black grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+    <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
     {
         tech.map(({id, src, title, style}) => (
         <div
         key={id} 
-        className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+        className={`shadow-md border-2 border-black  hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
             <img className="w-1/4 mx-auto" src={src} alt="" />
             <p className="mt-4">{title}</p>
         </div>
