becc22c2b172aef8b5ee36e27c840b9f5ea21cbc
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-24 17:41:52 -0400

    Update Experience.jsx

--- src/componets/Experience.jsx
+++ src/componets/Experience.jsx
@@ -118,7 +118,7 @@ const Experience = () => {
         <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
         <p className="py-6">These are technologies I frequently use! </p>
     </div>
-    <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+    <div className="w-full border-2 border-black grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
     {
         tech.map(({id, src, title, style}) => (
         <div
