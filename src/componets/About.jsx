67e8e973dee5eeba27b0eedcf6f895a520018346
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-24 17:08:47 -0400

    Update About.jsx

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -11,32 +11,13 @@ const About = () => {
 
     <div>
         <p className='text-xl mt-10'>
-        In January 2021, I joined my university's branch of the Association of Information Technology Professionals 
-        (AITP) as a Project Lead Developer. Leading a team, we designed and developed a custom application UI and UX, 
-        creating specialized content and documentation. Our project focused on developing a Covid-19 vaccination web 
-        application to assist the elderly and others. We worked diligently, adopting an agile approach to beat the 
-        state of Connecticut and gained recognition from major news agencies.
-        </p>
-        <p className='text-xl mt-4'>
-        In May 2021, I obtained a distinguished bachelor's degree in Business-Information-Systems, enhancing my 
-        technological skills and cultivating leadership and analytical thinking. Shortly after graduating, I worked 
-        as a Marketing Technology Specialist for a large law firm. After eight months, I realized the need to transition 
-        into a more technically focused or developer-friendly position.
-        </p>
-        <p className='text-xl mt-4'>
-        In 2022-2023, I worked as a Technical Project Consultant for a startup venture backed by Arcline Investments 
-        and later joined Comcast. These roles involved developing bespoke industrial ERP systems and mobile applications 
-        for elevator companies, as well as coordinating telecom infrastructure projects. Leveraging SaaS technologies, 
-        I streamlined processes, facilitated joint ventures, and provided comprehensive status reports.
-        </p>
-        <p className='text-xl mt-4'>
-        In 2023, I earned the AWS Certified Cloud Practitioner certification, demonstrating my proficiency in cloud 
-        technologies. Allowing me to guide teams through technical challenges in software development, cloud migration initiatives, 
-        workload architecture reviews, and mitigating high-risk issues. Skilled at bridging the gap between 
-        technical and business aspects of cloud initiatives. As an independent developer, I actively engage in projects utilizing 
-        technologies like React Native, Tailwind CSS, MERN stack, Vite, MySQL, NoSQL, and AWS services. My GitHub repository showcases my 
-        hands-on experience and growth as a developer, specializing in building Full-Stack web applications integrating
-         NoSQL, MySQL, and cloud technologies.
+         As a Business Information Systems Graduate, Software Developer, and AWS Certified Cloud Practitioner, 
+          I excel in guiding teams through technical challenges, cloud migration initiatives, workload architecture reviews, 
+          and mitigating high-risk issues. My expertise bridges the gap between the technical and business aspects of projects. 
+          With a focus on Full-Stack engineering, I actively engage in personal projects using various technologies such as MERN, 
+          Vite, ReactJS, NodeJS, C#, Python, SQL, HTML, CSS, Tailwindcss, SaSS, MySQL, MongoDB, DynamoDB, NoSQL, IAM, RBAC, AI, and various 
+          AWS services. My GitHub repositories demonstrate my passion for continuous growth, endless learning, and professional development, 
+          showcasing hands-on experiences and efficiently building secure, reliable projects with cloud technologies.
         </p>
     </div>
   </div>
