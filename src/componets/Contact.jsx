896e80545cd70ddbe05e2103aca4dec73a5954c8
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-07-24 17:18:54 -0400

    Update Contact.jsx

--- src/componets/Contact.jsx
+++ src/componets/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name="contact" className='w-full h-screen bg-gradient-to-r from-gray-900  to-indigo-900 p-4 text-white'>
+    <div name="contact" className='w-full h-screen bg-gradient-to-r from-black  to-gray-900 p-4'>
         <div className='flex flex-col p-8 justify-center max-w-screen-lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
