30cd49b8758af6f35af797f567f851d6e7099047
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-08-14 11:40:25 -0400

    Update About.jsx

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -3,22 +3,29 @@ import React from 'react';
 const About = () => {
 
   return (
-  <div name="about" className='w-full p-8 pt-10 pb-20 text-white'>
+  <div name="about" className='w-full p-8 pt-10 pb-20'>
     <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+          <p className='text-4xl text-black font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-
-    <div>
-        <p className='text-xl mt-10'>
-         As a Business Information Systems Graduate, Software Developer, and AWS Certified Cloud Practitioner, 
-          I excel in guiding teams through technical challenges, cloud migration initiatives, workload architecture reviews, 
-          and mitigating high-risk issues. My expertise bridges the gap between the technical and business aspects of projects. 
-          With a focus on Full-Stack engineering, I actively engage in personal projects using various technologies such as MERN, 
-          Vite, ReactJS, NodeJS, C#, Python, SQL, HTML, CSS, Tailwindcss, SaSS, MySQL, MongoDB, DynamoDB, NoSQL, IAM, RBAC, AI, and various 
-          AWS services. My GitHub repositories demonstrate my passion for continuous growth, endless learning, and professional development, 
-          showcasing hands-on experiences and efficiently building secure, reliable projects with cloud technologies.
+    <div classname="text-black">
+      {/* paragraph one */}
+      <p className='text-xl mt-10'>
+          As a Business Information Systems Graduate, Full-Stack Developer, and AWS Certified Cloud Practitioner, 
+          I excel in navigating teams through technical challenges, cloud migration initiatives, workload architecture 
+          examinations, and high-risk issue mitigation. My proficiency bridges the gap between technical and business dimensions.
+      </p>
+      {/* paragraph two */}
+      <p className='text-xl mt-10'>
+          I actively engage in personal projects, courses, and certifications utilizing a spectrum of technologies including MERN, 
+         Vite, ReactJS, NodeJS, C#, Python, SQL, HTML, CSS, Tailwindcss, SaSS, MySQL, MongoDB, DynamoDB, NoSQL, IAM, Cyber 
+         Security, AI, and assorted AWS services.
         </p>
+      {/* paragraph three */}
+      <p className='text-xl mt-10'>
+          My GitHub repositories epitomize my dedication to continuous growth, perpetual learning, and professional advancement. 
+         They showcase hands-on experience and adeptness in constructing secure, reliable projects with cloud technologies.
+      </p>
     </div>
   </div>
   </div>
