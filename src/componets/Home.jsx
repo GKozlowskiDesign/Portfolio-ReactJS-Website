8fa75d4e31573d673ad1a2e690d2439eb55fc3f3
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-08-14 11:48:33 -0400

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-{/* import Gary from '../assets/GaryK-1.jpg' */}
 const Home = () => {
   return (
   <div name="home" className='w-full bg-white pt-40 pb-40'>     
@@ -8,10 +7,7 @@ const Home = () => {
             <h2 className='lg:text-8xl text-6xl text-black px-4 py-4'>GKDesign</h2> 
         </div>
         <div className='px-12 py-12 m-4 p-4 rounded-xl  ring-gray-900'>
-          {/* <img src={Gary} alt="My Profile" 
-              class="rounded-xl"
-              className="rounded-xl  border-2 mx-auto w-4/6 lg:w-3/6"
-            /> */}
+
         </div>
     </div>  
   </div>
