d99d0ffc0f7e1bb8a54db4107ee5719d95d9df3a
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2023-08-14 11:43:49 -0400

    Update Home.jsx

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   <div name="home" className='w-full bg-white pt-40 pb-40'>     
     <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full p-10 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='lg:text-8xl text-6xl font-signature text-black px-4 py-4'>GKDesign</h2> 
+            <h2 className='lg:text-8xl text-6xl text-black px-4 py-4'>GKDesign</h2> 
         </div>
         <div className='px-12 py-12 m-4 p-4 rounded-xl  ring-gray-900'>
           {/* <img src={Gary} alt="My Profile" 
